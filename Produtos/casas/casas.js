f2f54e53d2f8bed9bf78b43e0efe4e86b89fdd5c
Matheus Cunha <matheusbabinski123@gmail.com>
2025-11-29 13:07:58 -0300

    Att paginas produtos

---
+++ Produtos/casas/casas.js
@@ -0,0 +1,7 @@
+// --- DROPDOWN --- //
+const dropBtn = document.querySelector("#dropBtn");
+const dropdown = document.querySelector(".dropdown");
+
+dropBtn.addEventListener("click", () => {
+    dropdown.classList.toggle("open");
+});
